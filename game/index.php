d8bc8658438fca549574dced96f7a4f8ff13c1e9
Ana Maria <115229769+ana-maria-barbosa@users.noreply.github.com>
2022-12-08 07:34:31 -0300

    Adicionando arquivos

---
+++ game/index.php
@@ -0,0 +1,112 @@
+<?php
+
+include('menu.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial - Meus Games</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="product.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+  <div class="album py-5 bg-light">
+    <div class="container">
+        <h1>Página Inicial - Meus Games</h1> <br>
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="imagens\minecraft.jpg" alt="10" class="bd-placeholder-img card-img-top">
+            <div class="card-body">
+              <p class="card-text">Minecraft é um jogo eletrônico sandbox de sobrevivência criado pelo desenvolvedor sueco Markus "Notch" Persson e posteriormente desenvolvido e publicado pela Mojang Studios, cuja propriedade intelectual foi obtida pela Microsoft em 2014. </p>
+              <div class="d-flex justify-content-between align-items-center">
+               
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src="imagens\Call-Of-Duty.jpg" alt="10" class="bd-placeholder-img card-img-top">
+
+            <div class="card-body">
+              <p class="card-text">Call of Duty é uma franquia de jogos eletrônicos de tiro em primeira pessoa publicado pela Activision.O primeiro título da série foi lançado em 2003 e começou nos computadores, mais tarde a série se expandiu para os mais variados consoles, portáteis e smartphones.</p>
+              <div class="d-flex justify-content-between align-items-center">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="imagens\GTA.jpg" alt="10" class="bd-placeholder-img card-img-top">
+            <div class="card-body">
+              <p class="card-text">Grand Theft Auto Online é um jogo eletrônico de ação-aventura desenvolvido pela Rockstar North e publicado pela Rockstar Games. Foi lançado em 1 de outubro de 2013 para PlayStation 3 e Xbox 360,em 18 de novembro de 2014 para PlayStation 4 e Xbox One, em 14 de abril de 2015 para Microsoft Windows, e em 15 de março de 2022 para PlayStation 5 e Xbox Series X/S.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="imagens\god_of_war.jpg" alt="10" class="bd-placeholder-img card-img-top">
+            <div class="card-body">
+              <p class="card-text">God of War é um jogo eletrônico de ação-aventura e hack and slash desenvolvido pela Santa Monica Studio e publicado pela Sony Computer Entertainment (SCE). Lançado pela primeira vez em 22 de março de 2005 para PlayStation 2, é o primeiro título da série com o mesmo nome e o terceiro cronologicamente.</p>
+              <div class="d-flex justify-content-between align-items-center">
+              
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+            
+          <div class="card shadow-sm">
+          <img src="imagens\Banana-Kong.jpg" alt="10" class="bd-placeholder-img card-img-top">
+            <div class="card-body">
+              <p class="card-text">O início de Banana Kong: No ano de 2012 , a empresa FDG ENTERTAINEMENT estava ficando sem ideias que poderiam ser interessantes, até que Thomas K ern chegou nos escritórios da FDG e teve a ideia de criar um endless runner com um gorila que foge de um tsunami de cascas de bananas, e o time curtiu muito a ideia e colocou a mão na massa .</p>
+              <div class="d-flex justify-content-between align-items-center">
+              
+              </div>
+            </div>
+          </div>
+        </div>
+    
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="imagens\Counter.jpg" alt="10" class="bd-placeholder-img card-img-top">
+        
+            <div class="card-body">
+            <p class="card-text">Counter-Strike (também abreviado por CS) é um popular jogo eletrônico de tiro em primeira pessoa.Inicialmente criado como um "mod" de Half-Life para jogos online, foi desenvolvido por Minh Le e Jess Cliffe e depois adquirido pela Valve Corporation. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</main>
+
+    
+    
+  
+
+
+
+  
+   
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</html>
